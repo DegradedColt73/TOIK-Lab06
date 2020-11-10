226d06bbab4c3466ada2e0afe7b9b4e54ebc0959
Mieszko Maciura <DegradedColt73@outlook.com>
2020-11-10 14:03:28 +0100

    implement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,45 @@
 package com.company;
 
-public class Main {
+class Main {
 
+    /**
+     * metoda main powinna implementowac algorytm do
+     * jak najszybszego wyszukiwania wartosci
+     * zmiennej digit z klasy QuizImpl (zakladamy ze
+     * programista nie zna zawartosci klasy QuizImpl).
+     * Nalezy zalozyc, ze pole digit w klasie QuizImpl
+     * moze w kazdej chwili ulec zmianie. Do wyszukiwania
+     * odpowiedniej wartosci nalezy wykorzystywac tylko
+     * i wylacznie metode isCorrectValue - jesli metoda
+     * przestanie rzucac wyjatki wowczas mamy pewnosc ze
+     * poszukiwana zmienna zostala odnaleziona.
+     */
     public static void main(String[] args) {
-	// write your code here
+
+        Quiz quiz = new QuizImpl();
+        int digit = (Quiz.MAX_VALUE - Quiz.MIN_VALUE) / 2 ; // zainicjuj zmienna
+        int min = Quiz.MIN_VALUE;
+        int max = Quiz.MAX_VALUE;
+
+        for(int counter = 1; ;counter++) {
+            try {
+                quiz.isCorrectValue(digit);
+                System.out.println("Trafiona proba!!! Szukana liczba to: "
+                        + digit + " Ilosc prob: " + counter);
+                break;
+            } catch(Quiz.ParamTooLarge e) {
+                System.out.println("Argument za duzy!!!");
+                max = digit;
+                digit = (max + min) / 2;
+                System.out.println(digit);
+                // implementacja logiki...
+            } catch(Quiz.ParamTooSmall e) {
+                min = digit;
+                digit = (max + min) / 2;
+                System.out.println("Argument za maly!!!");
+                System.out.println(digit);
+                // implementacja logiki...
+            }
+        }
     }
 }
--- src/com/company/QuizImpl.java
+++ src/com/company/QuizImpl.java
@@ -1,4 +1,21 @@
 package com.company;
 
-public class QuizImpl {
+class QuizImpl  implements Quiz {
+
+    private int digit;
+
+    public QuizImpl() {
+        this.digit = 254;    // zmienna moze ulegac zmianie!
+    }
+
+    @Override
+    public void isCorrectValue(int value) throws ParamTooLarge, ParamTooSmall {
+        if(value < this.digit){
+            throw new ParamTooSmall();
+        }
+        else if(value > this.digit){
+            throw new ParamTooLarge();
+        }
+        else return;
+    }
 }
